5c0998458a80dd88e8e179e153b96cd80612e199
Pavani_Gunna <pavani.gunna@gmail.com>
2024-10-05 21:19:09 +0100

    Amx trip planner interview test

--- AmxTripPlanner/FlightSearchViewController.swift
+++ AmxTripPlanner/FlightSearchViewController.swift
@@ -14,4 +14,39 @@ class FlightSearchViewController: UIViewController {
         // Do any additional setup after loading the view.
 
     }
+    
+    
+    @IBOutlet weak var fromTextField: UITextField!
+    
+    @IBOutlet weak var toTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var availableFlights: [Flight] = []
+    var mockDestinations = ["London", "New York", "Delhi", "Paris", "Frankfurt", "Amsterdam",  "Copenhagen"]
+    
+    
+    @IBAction func searchFlightsButtonTapped(_ sender: UIButton) {
+        let from = fromTextField.text ?? ""
+        let to = toTextField.text ?? ""
+        let date = datePicker.date
+        availableFlights = searchFlights(from: from, to: to, date: date)
+    }
+    
+func searchFlights(from: String, to: String, date: Date) -> [Flight] {
+        return [
+            Flight(flightNumber: "AA101", from: from, to: to, departureTime: "09:00 AM", arrivalTime: "12:00 PM"),
+            Flight(flightNumber: "DL203", from: from, to: to, departureTime: "11:00 AM", arrivalTime: "02:00 PM"),
+        ]
+    }
+    
+    
+}
+
+struct Flight {
+    var flightNumber: String
+    var from: String
+    var to: String
+    var departureTime: String
+    var arrivalTime: String
 }
