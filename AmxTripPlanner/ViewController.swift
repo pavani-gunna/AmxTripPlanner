652e5433e167862c203b7ac9191ad20beffb2399
Pavani_Gunna <pavani.gunna@gmail.com>
2024-10-05 20:48:52 +0100

    Amx trip planner interview test

--- AmxTripPlanner/ViewController.swift
+++ AmxTripPlanner/ViewController.swift
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
     }
 
 
+    @IBOutlet weak var userIdTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        let userId = userIdTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+
+        if validateLogin(userId: userId, password: password) {
+            errorLabel.isHidden = false
+            errorLabel.text = "valid User ID or Password"
+        } else {
+            errorLabel.isHidden = false
+            errorLabel.text = "Invalid User ID or Password"
+        }
+    }
+    
+    func validateLogin(userId: String, password: String) -> Bool {
+            return userId == "admin" && password == "password123"
+        }
+    
+    
 }
 
