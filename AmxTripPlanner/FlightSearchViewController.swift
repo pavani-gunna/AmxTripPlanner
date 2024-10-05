704da1a405887b10d4fcbaf693e0f43765a870ba
Pavani_Gunna <pavani.gunna@gmail.com>
2024-10-05 21:08:58 +0100

    Amx trip planner interview test

---
+++ AmxTripPlanner/FlightSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FlightSearchViewController.swift
+//  AmxTripPlanner
+//
+//  Created by Pavani Gunna on 05/10/2024.
+//
+
+import UIKit
+
+class FlightSearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+}
